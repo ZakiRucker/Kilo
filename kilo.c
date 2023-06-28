e5adb44f565426a397f63c534353696ae92d5f64
Zaki Rucker <zakirucker@mac.com>
2023-06-28 22:28:24 +0200

    Shell file first commit

---
+++ kilo.c
@@ -0,0 +1,12 @@
+// 
+// file: kilo.c
+//
+// created by: Zaki Rucker
+// from: https://viewsourcecode.org/snaptoken/kilo/01.setup.html
+//
+// compile with: cc kilo.c -o kilo
+
+int main()
+{
+    return 0;
+}
